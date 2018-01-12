d6c5df299d37f6137f5d873f646f024a6f4ab14e
Cris Pope <cris.pope@codingzeal.com>
2018-01-11 18:28:47 -0800

    Create AudioFiles table

---
+++ db/migrate/20180111213616_create_audio_files.rb
@@ -0,0 +1,11 @@
+class CreateAudioFiles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :audio_files do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    
+    add_attachment :audio_files, :audio
+  end
+end
