ed4f3731744b04c9dcd60443f58e81e6f081c6b8
learn <learn@learn01.local>
2018-01-18 09:52:13 -0800

    Create tests for valid audio formats

--- spec/features/upload_audio_file_spec.rb
+++ spec/features/upload_audio_file_spec.rb
@@ -12,13 +12,23 @@ RSpec.feature 'upload an audio file' do
     end
   end
 
-  context 'saves a file to the database' do
-    it 'creates an audio record and uploads the file to storage' do
+  context 'when the file is a valid wav' do
+    it 'creates an audio record and saves the file to storage' do
       visit '/'
       attach_file 'audio_file[audio]', support_file('test-file.wav')
-      expect{
+      expect {
         click_button "Start Transcription"
       }.to change(AudioFile, :count).by(1)
+    end
+  end
+
+  context 'when the file is NOT a valid wav' do
+    it 'will not create an audio record and save the file to storage' do
+      visit '/'
+      attach_file 'audio_file[audio]', support_file('test-file.mp3')
+      expect {
+        click_button "Start Transcription"
+      }.to change(AudioFile, :count).by(0)
     end
   end
 end
