4e8009f7342e86715e669dd0799b9897345b2062
Cris Pope <cris.pope@codingzeal.com>
2018-01-29 13:57:24 -0800

    Add feature test for indication a submission has been made

---
+++ spec/features/indication_submission_has_been_made_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+def support_file(filename)
+  Rails.root.join("spec/support/#{filename}")
+end
+
+RSpec.feature 'see an indication that the submission has been made' do
+  before do
+    visit '/'
+    attach_file 'audio_file[audio]', support_file('test-file.wav')
+    fill_in('audio_file[title]', with: 'test title')
+    click_button "Start Transcription"
+  end
+
+  context 'when a valid submission has been made' do
+    it 'displays the success message' do
+      expect(page).to have_content '"test title" transcription sent'
+    end
+  end
+end
