99098653418221ce0c16bb3c3201d314ef503990
Cris Pope <cris.pope@codingzeal.com>
2018-01-29 13:56:38 -0800

    Refactor code to use new syntax and to avoid duplication

--- spec/controllers/give_the_transcription_a_title_spec.rb
+++ spec/controllers/give_the_transcription_a_title_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AudioFile, :type => :model do
+RSpec.describe AudioFile, type: :model do
   it "is valid and can be given a title" do
     audio_file = AudioFile.new(title: 'Test Title')
     expect(AudioFile.new).to be_valid
