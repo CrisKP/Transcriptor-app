470692f43efc7f0b3c6b50c15a74f27b662b2ecc
Adam Cuppy <adam@codingzeal.com>
2018-01-29 08:57:24 -0800

    [#154618242] Add AudioFile#transcripton to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118192117) do
+ActiveRecord::Schema.define(version: 20180126060032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180118192117) do
     t.integer "audio_file_size"
     t.datetime "audio_updated_at"
     t.string "title"
+    t.binary "transcription"
+    t.datetime "transcribed_at"
   end
 
 end
