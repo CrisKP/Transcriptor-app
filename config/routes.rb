8a79e6c51def51ecd884e753098b985be3589c43
Adam Cuppy <adam@codingzeal.com>
2018-01-30 22:26:25 -0800

    [#154618222] Add HTTP Basic Auth for Sidekiq dashboard when in production

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,19 @@ Rails.application.routes.draw do
   root 'main#index'
 
   post '/index' => 'main#create'
-  mount Sidekiq::Web => '/sidekiq'
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    def hex(value)
+      ::Digest::SHA256.hexdigest value
+    end
+
+    def compare(left, right)
+      ActiveSupport::SecurityUtils.secure_compare(left, right)
+    end
+
+    compare(hex(username), hex(ENV["SIDEKIQ_USERNAME"])) &
+      compare(hex(password), hex(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+
+  mount Sidekiq::Web, at: '/sidekiq'
 end
