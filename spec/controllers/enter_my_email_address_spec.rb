ef72d63fdbcbab3c35974d2fa920d7bfd8d4ff66
Cris Pope <cris.pope@codingzeal.com>
2018-01-31 09:31:16 -0800

    Refactor controller test code

--- spec/controllers/enter_my_email_address_spec.rb
+++ spec/controllers/enter_my_email_address_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe AudioFile, type: :model do
   it "is valid and has a valid email" do
     audio_file = AudioFile.new(email: 'hello@email.com')
-    expect(AudioFile.new).to be_valid
+    expect(audio_file).to be_valid
     expect(audio_file.email).to eq('hello@email.com')
   end
 end
