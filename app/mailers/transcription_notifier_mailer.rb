90966437c10939f582e83eaf688a49b24c3d0240
Austin Gilinsky <austin.gilinsky@codingzeal.com>
2018-02-02 10:04:28 -0800

    Update active mailer setup

--- app/mailers/transcription_notifier_mailer.rb
+++ app/mailers/transcription_notifier_mailer.rb
@@ -1,6 +1,4 @@
 class TranscriptionNotifierMailer < ApplicationMailer
-  default :from => 'any_from_address@example.com'
-
    def transcription_email(audio_file)
      @audio_file = audio_file
      mail( :to => @audio_file.email,
