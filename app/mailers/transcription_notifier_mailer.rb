7c5d0a0124dbc0d373f09ffdf27d6d0293d754e3
learn <learn@learn01.local>
2018-02-02 17:26:59 -0800

    Create and deliver attachment

--- app/mailers/transcription_notifier_mailer.rb
+++ app/mailers/transcription_notifier_mailer.rb
@@ -1,7 +1,12 @@
 class TranscriptionNotifierMailer < ApplicationMailer
    def transcription_email(audio_file)
      @audio_file = audio_file
-     mail( :to => @audio_file.email,
-     :subject => 'Transcription completed' )
+
+     attachments["#{@audio_file.title}.txt"] = {
+       mime_type: 'text/plain',
+       content: @audio_file.transcription
+     }
+
+     mail to: @audio_file.email, subject: 'Transcription completed'
    end
 end
